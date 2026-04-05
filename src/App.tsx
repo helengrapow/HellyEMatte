8338c17483b77a45850bdf795d2cc211f9f53324
hgrapow <helen.grapow@ugent.be>
2026-04-05 16:21:34 +0200

    domain3

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/HellyEMatte">
+      <BrowserRouter basename="/">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/logistics" element={<Story />} />
