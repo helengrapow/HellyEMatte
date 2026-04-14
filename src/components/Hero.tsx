13f296d1077f311c0d5eac93333f0e0615ef43c7
hgrapow <helen.grapow@ugent.be>
2026-04-14 13:29:15 +0200

    links7

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -21,6 +21,7 @@ const Hero = () => {
         <div className="animate-fade-in">
           <h1 className="font-serif text-5xl md:text-7xl lg:text-8xl font-light mb-10 tracking-wide">
           <br />
+          <br />
             Helli <span className="text-pale-blue">&</span> Matte
             <br />
             <br />
