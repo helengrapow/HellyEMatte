5708aa27c1de17829968bc1207b769ebacb7671c
hgrapow <helen.grapow@ugent.be>
2026-04-14 13:51:52 +0200

    linkswithoutscroll

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -75,14 +75,6 @@ const Hero = () => {
         </div>
         </div>
       </div>
-     
-      {/* Scroll Indicator */}
-      <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 text-white animate-bounce">
-        <div className="w-6 h-10 border-2 border-white/50 rounded-full flex justify-center">
-          <div className="w-1 h-3 bg-white/50 rounded-full mt-2 animate-pulse" />
-        </div>
-      </div>
-    </div>
   );
 };
 
